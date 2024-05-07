c8e684e19a901f54bdc8b256532517cdf37a5ef0
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2024-05-07 22:04:44 +1000

    chore: Add AsyncStorage for storing theme preference

--- context/theme.js
+++ context/theme.js
@@ -1,4 +1,5 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ThemeContext = createContext();
 
@@ -6,6 +7,24 @@ export const useTheme = () => useContext(ThemeContext);
 
 export function ThemeProvider({ children }) {
   const [isLargeText, setIsLargeText] = useState(false);
+  useEffect(() => {
+    const getTheme = async () => {
+      try {
+        const storedTheme = await AsyncStorage.getItem("isLargeText");
+        if (storedTheme !== null) {
+          console.log("Stored theme: ", storedTheme);
+
+          setIsLargeText(JSON.parse(storedTheme));
+        } else {
+          setIsLargeText(false);
+        }
+      } catch (error) {
+        console.error("Error loading theme:", error);
+      }
+    };
+    setIsLargeText(false);
+    getTheme();
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ isLargeText, setIsLargeText }}>
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -3,17 +3,24 @@ import { useState } from "react";
 import { GlobalLayout } from "../components/Layout";
 import { useTheme } from "../context/theme";
 import { GlobalStyles } from "../styles/global";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function SettingsScreen() {
-    const { isLargeText, setIsLargeText } = useTheme();
-    const globalStyles = GlobalStyles();
+  const { isLargeText, setIsLargeText } = useTheme();
+  const globalStyles = GlobalStyles();
 
   return (
     <GlobalLayout>
       <View style={styles.view}>
         <Switch
           value={isLargeText}
-          onValueChange={() => setIsLargeText(!isLargeText)}
+          onValueChange={ async () => {
+            setIsLargeText(!isLargeText);
+            await AsyncStorage.setItem(
+              "isLargeText",
+              JSON.stringify(!isLargeText)
+            );
+          }}
           trackColor={{ false: "#767577", true: "#81b0ff" }}
         />
         <Text style={globalStyles.text}>Large Text</Text>
